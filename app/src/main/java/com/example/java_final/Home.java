367fa713c64b0ecea1e588d39247504327e8b505
Umut Sayar <umutsayar8@gmail.com>
2024-01-20 16:44:08 +0300

    Project Editted

--- app/src/main/java/com/example/java_final/Home.java
+++ app/src/main/java/com/example/java_final/Home.java
@@ -11,32 +11,13 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Home extends AppCompatActivity {
 
     TextView deneme_text;
-    Button addPhoto;
-    Button addLabel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
 
         deneme_text = findViewById(R.id.deneme_text);
-        addPhoto = findViewById(R.id.add_photo);
 
-        addPhoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(Home.this,AddPhotoFragment.class);
-                startActivity(i);
-            }
-        });
-
-        addLabel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(Home.this,AddLabelFragment.class);
-                startActivity(i);
-            }
-        });
     }
-
 
 }
